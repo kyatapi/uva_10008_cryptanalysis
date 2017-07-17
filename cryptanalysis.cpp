cdcfca867698e09a49f79cbefa96b5ceb8da36dc
Mike Su <kyatapi@gmail.com>
2017-07-17 22:03:22 +0800

    first draft

---
+++ cryptanalysis.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    size_t line_count;
+    cin >> line_count;
+    cin.ignore(1, '\n');
+
+    vector< tuple<char, size_t> > appear_times(26);
+    for (char i = 0; i < 26; ++i) {
+        get<0>(appear_times[i]) = i + 'A';
+        get<1>(appear_times[i]) = 0;
+    }
+
+    for (size_t l = 0; l < line_count; ++l) {
+        char token = '\0';
+        while (cin.get(token) && token != '\n') {
+            if (token >= 'a' && token <= 'z')  token = toupper(token);
+            else if (token < 'A' || token > 'Z') continue;
+
+            get<1>(appear_times[token - 'A'])++;
+        }
+    }
+
+    sort(appear_times.begin(), appear_times.end(), [](tuple<char, size_t> a, tuple<char, size_t> b) {
+        return (get<1>(a) == get<1>(b)) ? (get<0>(a) < get<0>(b)) : (get<1>(a) > get<1>(b));
+    });
+
+    for (size_t i = 0; i < 26 && get<1>(appear_times[i]) > 0; ++i) {
+        cout << get<0>(appear_times[i]) << " " << get<1>(appear_times[i]) << endl;
+    }
+
+    return 0;
+}
